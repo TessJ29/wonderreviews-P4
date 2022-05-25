723ff3407266b064c829fc25f4a0beae27468122
Tess Jansen <91196100+TessJ29@users.noreply.github.com>
2022-05-25 16:43:07 +0200

    Add files via upload
    front-page.php is de homepage en template-custom.php is voor nu het overzicht pagina

--- front-page.php
+++ front-page.php
@@ -17,7 +17,7 @@ render('views/templates/custom.php', compact('title', 'content'));
     <nav> 
         <ul>   
         <img src="http://www.wonder-reviews.local/wp-content/uploads/2022/05/logo_transparent.png" class="logo" alt="">   
-            <li><a href="http://www.wonder-reviews.local/homepage/">Home</a></li>
+            <li><a href="http://www.wonder-reviews.local/">Home</a></li>
             <li><a href="http://www.wonder-reviews.local/overzicht/ ">Overzicht</a></li>
         </ul>
     </nav>
