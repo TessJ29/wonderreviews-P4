070e0faf2717e36b34027c388a31bd6f609ea2a5
Tess Jansen <91196100+TessJ29@users.noreply.github.com>
2022-06-07 16:38:03 +0200

    Add files via upload
    Ik heb de homepage aangepast zodat de posts worden weergegeven.

--- single-reviews.php
+++ single-reviews.php
@@ -5,29 +5,30 @@
 
 // $newsCategories = get_the_terms(\get_post(), 'newsCategory');
 
-// render('views/templates/news-detail.php', compact('title','content', 'newsCategories'));
+render('views/templates/news-detail.php', compact('title','content', 'newsCategories'));
 
 
 ?>
 
 <!doctype html>
+
 <?php
 
 $post = get_post();
-$id = $post->ID;
+// $id = $post->ID;
 
+  $image = get_field('Image', get_post()->ID);
   $title = get_field('inputTitle', get_post()->ID);
   $content = get_field('inputContent', get_post()->ID);
   $score = get_field('Score', get_post()->ID);
   $url = get_field('inputURL', get_post()->ID);
+  // var_dump($image);
 ?>
-
-<div>
-    <div>
-        <img src="<?php echo $image['url'] ?>" alt="">
+<div class="container" id="singlepage">
+    <img src="<?php echo $image['url'] ?>" alt="">
     </div>
     <h1><?php echo $title ?></h1>
     <p><?php echo $content ?></p>
     <p>Score (<?php echo $score ?>)</p>
     <p>Url: <?php echo $url ?></p>
-</div>
+    </div>
--- template-overzicht.php
+++ template-overzicht.php
@@ -1,17 +1,18 @@
 <?php
 /*
- * Template Name: Home
+ * Template Name: Overzicht
  */
 
-// $title = get_the_title();
-// $content = get_the_content();
+$title = get_the_title();
+$content = get_the_content();
 
 render('views/templates/custom.php', compact('title', 'content'));
 
-$arguments = ['post_type' => 'reviews', 'numberposts' => 2, 'category' => 0, 'orderby' => 'date', 'order' => 'desc'];
-$listOfReviews = get_posts($arguments);
+
 ?>
 
+<!doctype html>
+<!-- Overzichts pagina -->
 <!doctype html>
 <header>
     <nav> 
@@ -22,37 +23,21 @@ $listOfReviews = get_posts($arguments);
         </ul>
     </nav>
 </header>
-<img src="http://www.wonder-reviews.local/wp-content/uploads/2022/05/homepage-banner-e1652883073752.png" class="banner" alt="">
-
-<h1>Laatste reviews</h1>
-
-<?php 
-  foreach($listOfReviews as $review) {
-  echo $review->post_title;
-}
-?>
-
 <div class="row">
-<?php
-  foreach($listOfReviews as $review): ?>
-  <?php
+    <div class="card">
+        <?php
         $image = get_field('Image', get_post()->ID);
         $title = get_field('inputTitle', get_post()->ID);
         $content = get_field('inputContent', get_post()->ID);
         $score = get_field('Score', get_post()->ID);
-        $url = get_field('inputUrl', get_post()->ID);
-        // var_dump($image);
-  ?>
+        $url = get_field('inputURL', get_post()->ID);
+        var_dump($image);
+        ?>
 
-  <div class="card">
-    <div class="col-sm">
-      <img src="<?php echo $image['url']; ?>">
+        <img class="singlepage-img" src= <?php echo $image['url'] ?> >
         <h5><?php echo $title ?></h5>
         <p><?php echo $content ?></p>
         <p><?php echo $score ?> </p>
         <p><?php echo $url ?></p>
-        <a href="<?php echo get_permalink($review); ?>">Lees meer</a>
-      </div>
     </div>
-  <?php endforeach ?>
-  </div>
+</div>
